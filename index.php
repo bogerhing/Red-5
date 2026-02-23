5ff51bb7a59b2426e4c7a81c8603aae8ecb1bc81
bogerhing <bogerhinglevd162@gmail.com>
2026-02-23 03:00:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,20 @@
-<?php
-  ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 <meta charset="UTF-8">
 <title>Red...</title>
 
 <script>
-fetch("https://ipwho.is/")
-  .then(response => response.json())
-  .then(data => {
-    if (!data.success) {
-      return;
-    }
+(function () {
+    const urlA = "https://google.com";
+    const urlB = "https://amazon.com";
 
-    const countryCode = data.country_code;
+    // 50-50 split
+    const target = Math.random() < 0.5 ? urlA : urlB;
 
-    if (countryCode === "JP") {
-      (function () {
-        const domainA = "https://starfish-app-lncr6.ondigitalocean.app/?Anph=(0101)-21735-08134";
-        const domainB = "https://coral-app-s7d92.ondigitalocean.app/?Anph=(0101)-21735-08134";
-
-        // Random number between 0 and 1
-        if (Math.random() < 0.5) {
-            window.location.replace(domainA);
-        } else {
-            window.location.replace(domainB);
-        }
-    })();
-    } else {
-      window.location.replace("https://www.amazon.com/");
-    }
-  })
-  .catch(error => {
-    console.error("Error fetching location:", error);
-  });
-
+    // Instant redirect (no history)
+    window.location.replace(target);
+})();
 </script>
 
 </head>
